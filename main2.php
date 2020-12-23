d5513afb5ccef90ae2d9d6fc6eb1a67f019e2df3
sja81 <sksja1981@gmail.com>
2020-12-23 01:04:35 +0100

    2nd tutorial file

--- main2.php
+++ main2.php
@@ -1,1 +1,14 @@
+<?php
+
+// PHP 7.1 features
 // Symmetric Array Destructuring
+
+$books = [
+    ['title' => 'The Martian', 'author' => 'Andy Weir'],
+    ['title' => 'Harry Potter', 'author' => 'JK Rowling']
+];
+
+foreach($books as ['title' => $title, 'author' => $author]) {
+    var_dump($title);
+    var_dump($author);
+}
