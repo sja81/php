3ded51b695659f0c8fbcaaebd6e277345a0296eb
sja81 <sksja1981@gmail.com>
2020-12-26 22:54:25 +0100

    new tutorial files

---
+++ solid principles.php
@@ -0,0 +1,16 @@
+<?php
+// SOLID
+
+// Single response
+// a class should have one, and only one, reason to change. 
+
+// Open-Closed
+// Entities should be open for extension, but closed for modification
+// Change behavior without modifying source code
+// avoid code rot
+// polymorphism
+// create hide extension to interface, like PaymentInterface
+
+// Liskov substitution
+// Derived classes must be substitutable for their base classes.
+// 
