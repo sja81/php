9c4005b8301dbb775e09f51eff29ae3e6deb791d
sja81 <sksja1981@gmail.com>
2020-12-26 16:00:26 +0100

    new tutorial files

--- main4.php
+++ main4.php
@@ -1,1 +1,29 @@
 <?php
+declare(strict_types=1); // !!!
+
+function setAge(int $age)
+{
+}
+
+setAge('30'); // PHP will translate '30' to integer 30!!!
+
+// The spaceship operator
+
+
+usort($games, function($a, $b){
+    return $a <=> $b; // -1, 0, 1
+    // $a < $b => -1
+    // $a = $b => 0
+    // $a > $b => 1
+});
+
+
+function sortBy($property)
+{
+    usort($users, function($uOne, $uTwo) use ($property) {
+        return $uOne->$property() <=> $uTwo->$property();
+    });
+}
+
+# Null coalesce operator
+
